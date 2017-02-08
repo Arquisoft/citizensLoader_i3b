26e1654c3d352873345ec58403cbd6a10d95a335
OriolInvernonLlaneza <93silf@gmail.com>
2017-02-08 19:52:41 +0100

    Added Citizen, excel parsing and password generation.

---
+++ src/main/java/es/uniovi/asw/parser/Citizen.java
@@ -0,0 +1,135 @@
+package es.uniovi.asw.parser;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Citizen {
+	private String name;
+	private String surname;
+	private String email;
+	private Date dob;
+	private String address;
+	private String dni;
+	private String password;
+	private String nationality;
+	private String nif;
+	private int ps;
+
+	public Citizen(String name, String surname, String email, String dob,
+			String address, String nationality, String dni, String NIF,
+			String pollingStation) {
+
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+		setDob(dob);
+		this.address = address;
+		this.nationality = nationality;
+		this.dni = dni;
+		this.nif = NIF;
+		this.ps = Integer.parseInt(pollingStation);
+	}
+
+	private void setDob(String dob) {
+		DateFormat format = new SimpleDateFormat("d/M/y");
+		Date date = null;
+		try {
+			date = format.parse(dob);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		this.dob = date;
+	}
+
+	public Citizen(String[] data) {
+		this.name = data[0];
+		this.surname = data[1];
+		this.email = data[2];
+		setDob(data[3]);
+		this.address = data[4];
+		this.nationality = data[5];
+		this.dni = data[6];
+		this.nif = data[7];
+		this.ps = Integer.parseInt(data[8]);
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String pw) {
+		this.password = pw;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public int getPs() {
+		return ps;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Citizen other = (Citizen) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Citizen [name=" + name + ", surname=" + surname + ", email="
+				+ email + ", dob=" + dob + ", address=" + address + ", dni="
+				+ dni + ", password=" + password + ", nationality="
+				+ nationality + ", nif=" + nif + ", ps=" + ps + "]";
+	}
+
+}
--- src/main/java/es/uniovi/asw/parser/ExcelReadList.java
+++ src/main/java/es/uniovi/asw/parser/ExcelReadList.java
@@ -2,6 +2,9 @@ package es.uniovi.asw.parser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -10,36 +13,60 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 public class ExcelReadList implements ReadList {
+	Set<Citizen> census;
+	private static final String passCharacters = "0123456789ABCDEFGHIJKLMNOP"
+			+ "QRSTUVWXYZabcdefghijklmnopqrstuvwxyz$-_¡!?¿@";
+	private static final int passLength = 12;
 
 	@Override
 	public void parse(String ruta) {
+		POIFSFileSystem fs;
+		HSSFWorkbook wb = null;
+		HSSFSheet sheet;
+		HSSFRow row;
+		HSSFCell cell;
 		try {
-			POIFSFileSystem fs = new POIFSFileSystem(
-					new FileInputStream(new File(ruta)));
-			HSSFWorkbook wb = new HSSFWorkbook(fs);
-			HSSFSheet sheet = wb.getSheetAt(0);
-			HSSFRow row;
-			HSSFCell cell;
+			fs = new POIFSFileSystem(new FileInputStream(new File(ruta)));
+			wb = new HSSFWorkbook(fs);
+			sheet = wb.getSheetAt(0);
+			census = new HashSet<Citizen>();
 
 			int rows = sheet.getPhysicalNumberOfRows();
 
-			int cols = 6; // Nombre/Apellidos/Email/Fecha
-							// nacimiento/Dirección/Nacionalidad/DNI
+			int cols = 9; // Nombre/Apellidos/Email/Fecha
+							// nacimiento/Dirección/Nacionalidad/DNI/NIF/Polling
+							// code
 
+			String[] data = new String[cols];
 			for (int r = 1; r < rows; r++) {
 				row = sheet.getRow(r);
 				if (row != null) {
 					for (int c = 0; c < cols; c++) {
 						cell = row.getCell((short) c);
 						if (cell != null) {
-							// TODO
+							data[c] = cell.toString();
 						}
 					}
 				}
+				Citizen cit = new Citizen(data);
+				createPassword(cit);
+				census.add(cit);
 			}
+			
+			wb.close();
 		} catch (Exception ioe) {
 			ioe.printStackTrace();
 		}
+	}
+
+	private void createPassword(Citizen cit) {
+		SecureRandom rnd = new SecureRandom();
+		StringBuilder sb = new StringBuilder(passLength);
+		for (int i = 0; i < passLength; i++) {
+			sb.append(passCharacters
+					.charAt(rnd.nextInt(passCharacters.length())));
+		}
+		cit.setPassword(sb.toString());
 	}
 
 }
