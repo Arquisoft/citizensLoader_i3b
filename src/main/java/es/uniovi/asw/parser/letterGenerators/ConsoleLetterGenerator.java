eaa510b30388eb8e3a1c539053bd62b0d63ba0ec
ORIOL INVERNON LLANEZA <UO245303@uniovi.es>
2017-02-13 10:12:15 +0100

    Added test, commented unused variable as suggested by Maven.

--- src/main/java/es/uniovi/asw/parser/letterGenerators/ConsoleLetterGenerator.java
+++ src/main/java/es/uniovi/asw/parser/letterGenerators/ConsoleLetterGenerator.java
@@ -11,13 +11,13 @@ public class ConsoleLetterGenerator implements LetterGenerator{
 
 	@Override
 	public void generatePersonalLetter(Citizen c) {
-		StringBuilder sb = new StringBuilder();
+		/*StringBuilder sb = new StringBuilder();
 		sb.append("To: "+c.getEmail()+"\n");
 		sb.append("Subject: Login data\n");
 		sb.append("Mr/Mrs "+ c.getName() +" "+ c.getSurname()+",\n\n");
 		sb.append("Your login data has been generated:\n");
 		sb.append("\tUsername: "+c.getEmail()+"\n");
-		sb.append("\tPassword: "+c.getPassword()+"\n");
+		sb.append("\tPassword: "+c.getPassword()+"\n");*/
 		System.out.println(c.getDni() +"letter sent.");
 	}
 
